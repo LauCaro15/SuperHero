e93da46302ca3bbcf840faab54dc9eefbf9908a9
LauCaro15 <laurac.candamilc@autonoma.edu.co>
2022-03-30 08:48:50 -0500

    Boom

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express()
 const routerApi = require("./src/routes");
+const { logErrors, errHandler ,boomErrorHandler} = require('./src/handlers/errors.handler');
+
 
 require('dotenv').config();
 const port = process.env.PORT;
 
 app.listen(port, ()=>console.log('Active port', port));
+app.use(logErrors);
+app.use(errHandler);
+app.use(boomErrorHandler);
 
 mongoose
     .connect(process.env.MONGODB_STRING_CONNECTION)
--- src/routes/superheroes.router.js
+++ src/routes/superheroes.router.js
@@ -5,48 +5,59 @@ const superheroSchema = require('../models/superheroModel');
 const service = new SuperheroService();
 /* POST: http://localhost:5000/api/v1/superheroes/superhero */
 superhero_routes.post('/superhero', async (req, res) => {
-  const superhero = superheroSchema(req.body);
-  await service
-    .createSuperhero(superhero)
-    .then((data) => res.status(201).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const superhero = superheroSchema(req.body);
+    const data = await service.createSuperhero(superhero)
+    res.status(201).json(data)
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 /* GET http://localhost:5000/api/v1/superheroes/ */
 superhero_routes.get('/', async (req, res) => {
-  await service
-    .listSuperhero()
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const data = await service.listSuperhero()
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 /* GET{:id}: hhtp://localhost:5000/api/v1/superheroes/superheroId*/
 superhero_routes.get('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  await service
-    .showSuperhero(superheroId)
-    .then((data) => res.status(302).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const data = await service.showSuperhero(superheroId)
+    res.status(302).json(data)
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 /* PUT{:id}: hhtp://localhost:5000/api/v1/superheroes/superheroId*/
 superhero_routes.put('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  const { superhero, real_name, features, superhero_sidekick } = req.body;
-  await service
-    .editSuperhero(
-      superheroId,
-      superhero,
-      real_name,
-      features,
-      superhero_sidekick
-    )
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(304).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const { superhero, real_name, features, superhero_sidekick } = req.body;
+    const data = await service
+      .editSuperhero(
+        superheroId,
+        superhero,
+        real_name,
+        features,
+        superhero_sidekick
+      )
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(304).json({ message: error });
+  }
 });
 /* DELETE{:id}: hhtp://localhost:5000/api/v1/superheroes/superheroId*/
 superhero_routes.delete('/:superheroId', async (req, res) => {
-  const { superheroId } = req.params;
-  await service
-    .removeSuperhero(superheroId)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+  try {
+    const { superheroId } = req.params;
+    const data = await service.removeSuperhero(superheroId)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 });
 module.exports = superhero_routes;
